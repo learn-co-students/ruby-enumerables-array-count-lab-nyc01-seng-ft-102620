b309c154f9673c07da4b915a0f08e9dc2ff66764
Josh Castro <castrojoshua805@gmail.com>
2020-10-09 01:11:30 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,11 +1,11 @@
 def count_strings(array)
   array.count do |words|
-   words.class == String
- end 
+    words.class == String 
+  end 
 end
 
 def count_empty_strings(array)
-  array.count do |words| 
-    words.class == String && words.length == 0   
+  array.count do |elements|
+    elements.class == String && elements.length == 0
   end 
-end
+end 
